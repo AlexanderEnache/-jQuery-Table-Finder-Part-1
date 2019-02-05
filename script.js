baf63cc04e59d947c287bad7bf8b0444a24c2547
Alex Enache <ragnarxon@gmail.com>
2019-02-05 08:59:58 -0500

    def

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 
 "use strict"
 
+class reserved{
+	
+	
+	
+}
+
+class available{
+	
+	
+	
+}
+
 $(document).ready(function () {
 	
 	console.log("sfwv");
+	
+	$(".table").addClass("available");
 
 	$(".close").on("click", function(e){
 		
@@ -13,9 +27,16 @@ $(document).ready(function () {
 		
 	});
 	
-	$(".submit").on("click", function(e){
+	$(".table").on("click", function(e){
 		
-		;
+		$(e.target).removeClass("available").addClass("reserved");
+		$(e.target).css("background-color", "#ADAAAA");
+		
+		$(".submit").on("click", function(e){
+			
+			$(".forum").css("display", "none");
+			
+		});
 		
 	});
 	
