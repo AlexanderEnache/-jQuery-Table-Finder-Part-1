6d2d53197901868876db29fa8d767a537c6f8c93
Alex Enache <ragnarxon@gmail.com>
2019-02-04 22:52:12 -0500

    Lab 8 part one, display only, no functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+
+"use strict"
+
+$(document).ready(function () {
+	
+	console.log("sfwv");
+
+	$(document).on("click", function(e){
+		
+		$(".forum").css("display", "none");
+		
+		console.log("close");
+		
+	});
+	
+});
