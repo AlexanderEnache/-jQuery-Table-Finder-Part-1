024ad117a36cad5c99df8777d1428ea443a530e1
Alex Enache <ragnarxon@gmail.com>
2019-02-05 23:10:01 -0500

    sdfgh

--- script.js
+++ script.js
@@ -1,45 +1,127 @@
 
 "use strict"
 
-class reserved{
+class Reservation{
 	
-	
-	
-}
-
-class available{
-	
-	
+	constructor(name, phone, num){
+		
+		this.name = name;
+		this.phone = phone;
+		this.num = num;
+		
+	}
 	
 }
 
 $(document).ready(function () {
 	
-	console.log("sfwv");
+	let tabs = new Array(9);
 	
 	$(".table").addClass("available");
-
+	
 	$(".close").on("click", function(e){
 		
-		$(".forum").css("display", "none");
-		
-		console.log("close");
+		$(".forum").fadeOut(1000);
 		
 	});
 	
 	$(".table").on("click", function(e){
 		
-		$(e.target).removeClass("available").addClass("reserved");
-		$(e.target).css("background-color", "#ADAAAA");
+		$(".forum").fadeIn(1000);
 		
-		$(".forum").css("display", "block");
+		$(".tableNum").html("Table number : " + e.target.id);
 		
-		$(".submit").on("click", function(e){
+		$(".submit").on("click", function(er){
 			
-			$(".forum").css("display", "none");
+			$(e.target).removeClass("available").addClass("reserved");
+			
+			$(".forum").fadeOut(1000);
+			
+			let id = Number(e.target.id);
+			
+			tabs[id - 1] = new Reservation($(".name").val(), $(".phone").val(), $(".num").val());
+			$("#h"+e.target.id).prepend(`Name: ${tabs[id-1].name}<br>Number of guests ${tabs[id-1].num}`);
 			
 		});
 		
 	});
 	
+	let offHover = true;
+	let offTable = true;
+	let id;
+	
+	$(".table").on("mouseenter", function(e){
+		
+		let id = Number(e.target.id);
+		
+		if(e.target.classList[1] == "reserved"){
+			
+			offTable = false;
+			
+			$("#h"+e.target.id).css("display", "block");
+		
+		}
+		
+	});
+	
+	$(".hovered").on("mouseenter", function(e){
+		
+		offHover = false;
+		
+	});
+	
+	$(".table").on("mouseout", function(e){
+		
+		//$("#"+e.target.id).html(e.target.id);
+		id = e.target.id;
+		
+		offTable = true;
+		
+		//console.log(offTable);
+		
+	});
+	
+	$(".hovered").on("mouseout", function(e){
+			
+		//$("#"+e.target.id).html(e.target.id);
+		
+		offHover = true;
+		
+		console.log("sadfghjk"+offHover);
+		
+	});
+	
+	$(document).mousemove(function(e){
+		
+		console.log("Shit");
+		
+		setTimeout(function(){
+			
+			if(offHover && offTable){
+				
+				$(".hovered").css("display", "none");
+				
+			}
+			
+		}, 1000)
+	  
+	});
+	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
